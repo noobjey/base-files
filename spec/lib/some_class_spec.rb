957fd5627cfc1eaebeb84a3ccce34e1b3532d701
Jason Wright <noobjey@gmail.com>
2015-05-24 12:34:52 -0600

    updated spec to use instance method

--- spec/lib/some_class_spec.rb
+++ spec/lib/some_class_spec.rb
@@ -1,9 +1,11 @@
 require './lib/some_class'
 
-describe some_class do
+describe SomeClass do
   describe 'some class behaviour' do
     it 'does this' do
-      expect(some_class.method()).to eq(true)
+      instance = SomeClass.new
+
+      expect(instance.some_method()).to eq(true)
     end
   end
 end
